07a7f69d9b4e229dc8101467e90e85f18559493d
edinpark <tonaco.braulio@gmail.com>
2018-12-25 16:17:15 -0500

    Added db.init_app() method to register its function with application

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -82,5 +82,9 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+    print(" * Registering with application - calling init_app(app)")
+    from . import db
+    db.init_app(app)
+
     print(" * Returning App")
     return app
